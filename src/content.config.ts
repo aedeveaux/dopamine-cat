c212ca28db3947910ed1e78c4b00262272a1aae5
aedeveaux <april.deveaux@gmail.com>
2026-03-17 21:55:02 -0400

    Add brain science template, update AiChat avatar from A to Me

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.string(),
-    type: z.enum(['brain science', 'tools', 'squirrels', 'story']).default('story'),
+    type: z.enum(['story', 'brain science', 'tools']).default('story'),
     tags: z.array(z.string()).default([]),
     excerpt: z.string(),
     readTime: z.string(),
